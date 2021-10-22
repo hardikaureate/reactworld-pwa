b53dcbe392a8db9b7808ccbb9a9c1c8b9378fde9
hardikaureate <hardik.devani@aureatelabs.com>
2021-10-22 12:35:58 +0530

    Pagination and Title update on Doc page

--- src/components/Doctable.js
+++ src/components/Doctable.js
@@ -1,35 +1,44 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import DataTable from 'react-data-table-component';
 import data from '../blogdata/tabledata.json';
 
 const Doctable = () => {
-    const columns = [
-        {
-            name: 'ID',
-            selector: 'name',
-            sortable: true,
-        },
-        {
-            name: 'Hooks/Function/Functionality',
-            selector: 'allitem',
-            sortable: true,
-        },
-        {
-            name: 'React',
-            selector: 'email',
-            //sortable: true,
-        },
-        {
-            name: 'Company',
-            selector: 'company',
-        },
-    ];
+    const [columns, setColumns] = useState([]);
+	const [pending, setPending] = useState(true);
+    useEffect(() => {
+		const timeout = setTimeout(() => {
+			setColumns([
+				{
+                    name: 'ID',
+                    selector: 'name',
+                    sortable: true,
+                },
+                {
+                    name: 'Hooks/Function/Functionality',
+                    selector: 'allitem',
+                    sortable: true,
+                },
+                {
+                    name: 'React',
+                    selector: 'email',
+                    //sortable: true,
+                },
+                {
+                    name: 'Company',
+                    selector: 'company',
+                },
+			]);
+			setPending(false);
+		}, 1500);
+		return () => clearTimeout(timeout);
+	}, []);
+
     return (
         <>
             <div id="wrapper">
                 <section id="one">
                     <div class="inner">
-                        <div class="row">
+                        <div class="row doctable">
                             <div class="col-12 col-12-small">
                                 <h2 style={{ textAlign: 'center', marginBottom: '50px' }}>Use React Hooks and Functionalities</h2>
                             </div>
@@ -41,6 +50,7 @@ const Doctable = () => {
                                         data={data}
                                         pagination
                                         highlightOnHover
+                                        progressPending={pending}
                                     />
                                 </div>
                             </div>
