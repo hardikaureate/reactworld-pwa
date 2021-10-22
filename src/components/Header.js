3b5965b44b63c9a7307e05af663adf8999a4e4fd
hardikaureate <hardik.devani@aureatelabs.com>
2021-10-22 10:28:43 +0530

    Apply Active class on Menu active Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { Icon, Box, Badge } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { MdShoppingCart } from 'react-icons/md'
 
@@ -43,27 +43,27 @@ const Header = () => {
                     </div>
                 </header> */}
                 <header id="header" className="header alt">
-                <div className="sitelogo"><Link to="/" className="logo"><strong>HD</strong> <span>by REACT</span></Link>{time}</div>
+                    <div className="sitelogo"><Link to="/" className="logo"><strong>HD</strong> <span>by REACT</span></Link>{time}</div>
                     <div className="miniheader">
-                    <input className="menu-btn" type="checkbox" id="menu-btn" />
-                    <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
-                    <ul className="menu">
-                    <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/portfolio">Portfolio</Link></li>
-                            <li><Link to="/shop">Shop</Link></li>
-                            <li><Link to="/post">Blog</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
+                        <input className="menu-btn" type="checkbox" id="menu-btn" />
+                        <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
+                        <ul className="menu">
+                            <li><Link to="/">Home</Link></li>
+                            <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/portfolio">Portfolio</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/shop">Shop</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/post">Blog</NavLink></li>
+                            <li><NavLink activeClassName="active" to="/contact">Contact</NavLink></li>
                             {width < 767 ?
                                 <li><Box className="mobilecarticon"><Icon className="cartIcon" onClick={() => openCart()} fill="#fff" cursor="pointer" as={MdShoppingCart} w={30} h={30}></Icon>
                                     <Badge backgroundColor="#f3eafd" borderRadius="50%" className="cartQuan">{checkout.lineItems?.length}</Badge></Box>
                                 </li>
-                            :
+                                :
                                 <li><Box><Icon onClick={() => openCart()} fill="#fff" cursor="pointer" as={MdShoppingCart} w={30} h={30}></Icon>
                                     <Badge backgroundColor="#f3eafd" borderRadius="50%" className="cartQuan">{checkout.lineItems?.length}</Badge></Box>
                                 </li>
                             }
-                    </ul>
+                        </ul>
                     </div>
                 </header>
             </div></>
