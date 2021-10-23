687f87c48fde24775effdba9382d995930a9842c
hardikaureate <hardik.devani@aureatelabs.com>
2021-10-23 14:20:21 +0530

    footer links update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                             <ul className="icons">
                                 <li><a href="https://twitter.com/hardik171192" target="_blank" className="icon brands alt fa-twitter"><span className="label">Twitter</span></a></li>
                                 <li><a href="https://www.facebook.com/hardik.2626/" target="_blank" className="icon brands alt fa-facebook-f"><span className="label">Facebook</span></a></li>
-                                <li><a href="https://www.instagram.com/hardikdevani/" target="_blank" className="icon brands alt fa-instagram"><span className="label">Instagram</span></a></li>
+                                <li><a href="https://www.instagram.com/" target="_blank" className="icon brands alt fa-instagram"><span className="label">Instagram</span></a></li>
                                 <li><a href="https://github.com/hardikaureate" target="_blank" className="icon brands alt fa-github"><span className="label">GitHub</span></a></li>
                                 <li><a href="https://in.linkedin.com/in/hardik-devani-4b9335b0" target="_blank" className="icon brands alt fa-linkedin-in"><span className="label">LinkedIn</span></a></li>
                             </ul>
